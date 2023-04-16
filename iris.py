df78342891202c01f72134df6312d97fba757200
Geraldo157 <gsmecanica76@gmail.com>
2023-04-16 14:45:51 -0300

    Ending of the iris dataset analisys

--- iris.py
+++ iris.py
@@ -29,3 +29,35 @@ dataset.hist()
 pyplot.show()
 scatter_matrix(dataset)
 pyplot.show()
+
+array = dataset.values
+x = array[:, 0: 4]
+y = array[:, 4]
+x_train, x_validation, y_train, y_validation = train_test_split(x, y, test_size=0.20, random_state=1)
+
+models = []
+
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+results = []
+names = []
+
+for name, model in models:
+    k_fold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+    cv_results = cross_val_score(model, x_train, y_train, cv=k_fold, scoring='accuracy')
+    results.append(cv_results)
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+model = SVC(gamma='auto')
+model.fit(x_train, y_train)
+predictions = model.predict(x_validation)
+
+print()
+print(accuracy_score(y_validation, predictions))
+print(confusion_matrix(y_validation, predictions))
+print(classification_report(y_validation, predictions))
