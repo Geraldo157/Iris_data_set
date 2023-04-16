40a4c18a9ece1909c5f918602dc5e01d15d6a22f
Geraldo157 <gsmecanica76@gmail.com>
2023-04-16 13:48:45 -0300

    Dataset visualization and description

--- iris.py
+++ iris.py
@@ -17,3 +17,15 @@ from sklearn.svm import SVC
 
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = read_csv('iris.csv', names=names)
+
+print(dataset.shape)
+print(dataset.head(20))
+print(dataset.describe())
+print(dataset.groupby('class').size())
+
+dataset.plot(kind='box', subplots=True, layout=(2, 2), sharex=False, sharey=False)
+pyplot.show()
+dataset.hist()
+pyplot.show()
+scatter_matrix(dataset)
+pyplot.show()
